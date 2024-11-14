77ce5fb272996972e587cca6797038cbc8869b36
wsilva <wsilva@fei.edu.br>
2024-11-14 17:16:54 -0300

    countdown init

--- quiz/main.py
+++ quiz/main.py
@@ -24,17 +24,19 @@ def main(page: ft.Page):
     style_button = ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10), padding=ft.padding.symmetric(vertical=25), color="white", text_style=ft.TextStyle(size=16))
     
     # Pergunta e alternativas
-    question = ft.Text("Qual é a capital da França?", text_align=ft.TextAlign.CENTER, size=16, weight=ft.FontWeight.BOLD)
-    question_container = ft.Container(content=question, margin=ft.margin.only(bottom=40))
+    question = ft.Text("Qual é a capital da França?", text_align=ft.TextAlign.CENTER, size=16, weight=ft.FontWeight.BOLD)    
 
     button1 = ft.ElevatedButton("Londres", width=page.width, style=style_button)
     button2 = ft.ElevatedButton("Paris", width=page.width, style=style_button)
     button3 = ft.ElevatedButton("Berlim", width=page.width, style=style_button)
     button4 = ft.ElevatedButton("Roma", width=page.width, style=style_button)   
 
+    countdown = countdown_canvas(page)
+
+    question_container = ft.Container(content=question, margin=ft.margin.only(bottom=40))
     buttons_columns = ft.Column(controls = [button1, button2, button3, button4])
     button_container = ft.Container(content=buttons_columns, margin=ft.margin.only(bottom=40))
-    countdown_container = ft.Container(content=countdown())
+    countdown_container = ft.Container(content=countdown, width=page.window.width, height=100, bgcolor="red")
     
     # Layout com a pergunta e os botões de resposta
     page.add(
@@ -46,22 +48,19 @@ def main(page: ft.Page):
     # Inicia a thread para destacar a resposta correta
     threading.Thread(target=highlight_correct_answer, args=(button2,), daemon=True).start()
 
-def countdown():
-    stroke_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE, color="white")
-    fill_paint = ft.Paint(style=ft.PaintingStyle.FILL, color="green")
+def countdown_canvas(page):
+    stroke_paint = ft.Paint(stroke_width=10, style=ft.PaintingStyle.STROKE, color="white")
+    fill_paint = ft.Paint(style=ft.PaintingStyle.FILL, color="green")    
+    center_x = (page.window.width - 50) / 2
     cp = cv.Canvas(
         [
-            cv.Circle(100, 100, 50, stroke_paint),
-            cv.Circle(80, 90, 10, stroke_paint),
-            cv.Circle(84, 87, 5, fill_paint),
-            cv.Circle(120, 90, 10, stroke_paint),
-            cv.Circle(124, 87, 5, fill_paint),
-            cv.Arc(70, 95, 60, 40, 0, math.pi, paint=stroke_paint),
+            #cv.Circle(center_x, 100 / 2, 50, stroke_paint),            
+            cv.Arc(center_x, 95, 50, 40, 0, math.pi, paint=stroke_paint),
         ],
         width=float("inf"),
+        height=1250,
         expand=True,
     )
-
     return cp
 
 # Função para exibir o pop-up com o resultado
