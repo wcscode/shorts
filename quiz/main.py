f1bff765f0ca40f9ea894e9e18fd15d37d7c6ae9
william <wileao@gmail.com>
2024-11-16 23:01:11 -0300

    transition

--- quiz/main.py
+++ quiz/main.py
@@ -18,7 +18,7 @@ def main(page: ft.Page):
     page.window.resizable = False  # Impede o redimensionamento da janela
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER    
 
     style_button = ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=10), padding=ft.padding.symmetric(vertical=25), color="white", text_style=ft.TextStyle(size=16))
     
@@ -32,27 +32,52 @@ def main(page: ft.Page):
 
     progress_bar = ft.ProgressBar(height=10,color="amber")   
 
-    question_container = ft.Container(content=question, margin=ft.margin.only(bottom=40))
+    question_container = ft.Container(content=question, width=page.width, margin=ft.margin.only(bottom=40))
     buttons_columns = ft.Column(controls = [button1, button2, button3, button4])
     button_container = ft.Container(content=buttons_columns, margin=ft.margin.only(bottom=40))
     progress_bar_container = ft.Container(content=progress_bar)
     
+    quiz_container = ft.Container(content=ft.Column(controls=[
+        question_container, button_container, progress_bar_container
+    ]))
+
+    transition_text = ft.Text("Daily Quiz", text_align=ft.TextAlign.CENTER, size=16, weight=ft.FontWeight.BOLD)
+    transition_container = ft.Container(content=transition_text)
+
+    main_container = set_animation(transition_container)
+
+    def animate():
+        main_container.content = transition_container if main_container.content == quiz_container else quiz_container
+        main_container.update()
     # Layout com a pergunta e os botões de resposta
-    page.add(
-        question_container,
-        button_container,
-        progress_bar_container
-    )
+    page.add(main_container)
+
+    time.sleep(1)
+    animate()
 
     progress_bar_update(page, progress_bar)
     highlight_correct_answer(button2)
+
+    time.sleep(1)
+    animate()
+
     # Inicia a thread para destacar a resposta correta
    # threading.Thread(target=highlight_correct_answer, args=(button2,), daemon=True).start()
+
+def set_animation(initial_container):
+    return ft.AnimatedSwitcher(
+        initial_container,
+        transition=ft.AnimatedSwitcherTransition.SCALE,
+        duration=1000,
+        reverse_duration=100,
+        switch_in_curve=ft.AnimationCurve.EASE_IN_OUT_CIRC,
+        switch_out_curve=ft.AnimationCurve.EASE_IN_OUT_CIRC,
+    )
 
 def progress_bar_update(page, progress_bar):
     for i in range(0, 101):
         progress_bar.value = i * 0.01
-        time.sleep(0.1)
+        time.sleep(0.05)
         page.update()
 
 # Função para exibir o pop-up com o resultado
